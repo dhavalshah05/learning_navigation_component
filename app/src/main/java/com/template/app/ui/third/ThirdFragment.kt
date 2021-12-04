5c6b9c50f3e6be5b83722b8b8357262a6943ca38
Dhaval Shah <dhaval@alphastack.in>
2021-12-04 17:03:06 +0530

    [+] Pop backstack and pop backstack to specific destination

--- app/src/main/java/com/template/app/ui/third/ThirdFragment.kt
+++ app/src/main/java/com/template/app/ui/third/ThirdFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.template.app.R
 import com.template.app.databinding.FragmentThirdBinding
 import com.template.app.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -20,6 +22,7 @@ class ThirdFragment : BaseFragment() {
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentThirdBinding.inflate(inflater, container, false)
+        initListeners()
         return binding.root
     }
 
@@ -28,4 +31,20 @@ class ThirdFragment : BaseFragment() {
         _binding = null
     }
 
+    private fun initListeners() {
+        binding.buttonGoBack.setOnClickListener {
+            goBack()
+        }
+        binding.buttonGoBackToFirstScreen.setOnClickListener {
+            goBackToFirstScreen()
+        }
+    }
+
+    private fun goBack() {
+        findNavController().popBackStack()
+    }
+
+    private fun goBackToFirstScreen() {
+        findNavController().popBackStack(R.id.fragmentFirst, false)
+    }
 }
