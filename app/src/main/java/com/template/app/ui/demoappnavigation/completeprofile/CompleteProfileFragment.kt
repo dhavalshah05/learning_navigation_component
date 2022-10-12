cd184214b17add5c673ecb6f519581888b2ad60c
Dhaval Shah <dhaval@alphastack.in>
2022-10-12 14:33:24 +0530

    Adding complete profile navigation

--- app/src/main/java/com/template/app/ui/demoappnavigation/completeprofile/CompleteProfileFragment.kt
+++ app/src/main/java/com/template/app/ui/demoappnavigation/completeprofile/CompleteProfileFragment.kt
@@ -1,26 +1,31 @@
-package com.template.app.ui.demoappnavigation.createaccount
+package com.template.app.ui.demoappnavigation.completeprofile
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatButton
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.findNavController
 import com.template.app.R
 import com.template.app.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class CreateAccountFragment : BaseFragment() {
+class CompleteProfileFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.create_account_fragment, container, false)
+        return inflater.inflate(R.layout.complete_profile_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         view.findViewById<AppCompatButton>(R.id.buttonNext).setOnClickListener {
-            findNavController().navigate(R.id.navigateActionCompleteProfile)
+            val options = NavOptions.Builder()
+                .setPopUpTo(findNavController().graph.first().id, true)
+                .build()
+            findNavController().navigate(R.id.navigateActionHome, null, options)
         }
     }
+
 }
