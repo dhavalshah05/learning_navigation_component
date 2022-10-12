f69c5acfd31501acde13fe468a417814dba321d2
Dhaval Shah <dhaval@alphastack.in>
2022-10-12 20:33:57 +0530

    Adding setting detail screen

--- app/src/main/java/com/template/app/ui/demoappnavigation/settings/SettingDetailFragment.kt
+++ app/src/main/java/com/template/app/ui/demoappnavigation/settings/SettingDetailFragment.kt
@@ -4,23 +4,18 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.widget.AppCompatButton
-import androidx.navigation.fragment.findNavController
 import com.template.app.R
 import com.template.app.ui.base.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class SettingsFragment : BaseFragment() {
+class SettingDetailFragment : BaseFragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.settings_fragment, container, false)
+        return inflater.inflate(R.layout.setting_detail_fragment, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        view.findViewById<AppCompatButton>(R.id.buttonDetail).setOnClickListener {
-            findNavController().navigate(R.id.navigateActionSettingDetail)
-        }
     }
 }
